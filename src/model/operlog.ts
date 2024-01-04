13295b6eebcf88a54995eb5c0c2692fb2cc5d7d5
zd <476182045@qq.com>
2024-01-04 21:59:57 +0800

    types

--- src/model/operlog.ts
+++ src/model/operlog.ts
@@ -2,5 +2,27 @@
  * 日志 数据模型
  */
 export interface OperlogModel {
-
+  createBy?: any;
+  createTime?: any;
+  updateBy?: any;
+  updateTime?: any;
+  remark?: any;
+  operId: number;
+  title: string;
+  businessType: number;
+  businessTypes?: any;
+  method: string;
+  requestMethod: string;
+  operatorType: number;
+  operName: string;
+  deptName: string;
+  operUrl: string;
+  operIp: string;
+  operLocation: string;
+  operParam: string;
+  jsonResult?: string;
+  status: number;
+  errorMsg?: any;
+  operTime: string;
+  costTime: number;
 }
