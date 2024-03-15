046574160a681b5d3b5052e802eace278edf6f87
zd <476182045@qq.com>
2024-03-15 15:57:20 +0800

    1

--- src/types/vue.d.ts
+++ src/types/vue.d.ts
@@ -1,13 +1,7 @@
+import 'vue';
 import type { VSay } from '@/directive/index';
 import type { AppGlobalProperties } from '@/utils/app/global-properties';
-import 'vue';
-// export interface Permission extends HTMLElement {
-//   [INSTANCE_KEY]?: {
-//     options: string[];
-//   };
-// }
 
-// type vPermission = Directive<Permission, string[]>;
 declare module 'vue' {
   interface ComponentCustomProperties extends AppGlobalProperties {
 
